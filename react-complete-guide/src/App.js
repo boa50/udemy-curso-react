9f897c0b0fd14862bad8755cfc6ab9441b3dc162
boa50 <bruno.albuquerque50@gmail.com>
2019-12-26 18:20:39 -0300

    Aula 76

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -41,20 +41,8 @@ class App extends Component {
   }
 
   render() {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color: 'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer',
-    //   ':hover': {
-    //     backgroundColor: 'lightgreen',
-    //     color: 'black'
-    //   }
-    // };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -70,6 +58,8 @@ class App extends Component {
         </div>
       );
 
+      btnClass = classes.Red;
+
       // style.backgroundColor = 'red';
       // style[':hover'] = {
       //   backgroundColor: 'salmon',
@@ -77,20 +67,19 @@ class App extends Component {
       // }
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Que legal</h1>
-        <p className={classes.join(' ')}>Que legal 2</p>
-        <button className='buttton'
-          // style={style}
+        <p className={assignedClasses.join(' ')}>Que legal 2</p>
+        <button className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
