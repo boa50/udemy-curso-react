94739a60ffb4570c482d6cc2a19c1bade8d8bfa0
boa50 <bruno.albuquerque50@gmail.com>
2020-02-14 10:56:49 -0300

    Aula 257

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,29 +89,17 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Boa 50',
-                address: {
-                    street: 'alguma',
-                    zipCode: '18237651',
-                    country: 'Moon'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'slowest'
+        
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&')
+        this.props.history.push({
+            pathname: './checkout',
+            search: '?' + queryString
+        });
     }
     
     render() {
